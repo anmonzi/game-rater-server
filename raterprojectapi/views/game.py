d0db7d5805e216c34b4f251ca3a4b9f50e89a8dc
Ashton Monzi <anmonzi@gmail.com>
2021-08-05 13:54:27 -0500

    added views for auth and game along with url routes

---
+++ raterprojectapi/views/game.py
@@ -0,0 +1,89 @@
+"""View module for handling requests about games"""
+from django.core.exceptions import ValidationError
+from rest_framework import status
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers
+from raterprojectapi.models import Game, GameCategory, Player
+
+
+
+class GameView(ViewSet):
+    """Game rater games"""
+
+    def created(self, request):
+        """Handle POST operations
+
+        Returns:
+            Response -- JSON serialized game instance
+        """
+
+        # Uses the token passed in the `Authorization` header
+        player = Player.objects.get(user=request.auth.user)
+
+        # Create a new Python instance of the Game class
+        # and set its properties from what was sent in the
+        # body of the request from the client.
+        game = Game()
+        game.title = request.data["title"]
+        game.description = request.data["description"]
+        game.designer = request.data["designer"]
+        game.year_released = request.data["year_released"]
+        game.number_of_players = request.data["number_of_players"]
+        game.game_time = request.data["game_time"]
+        game.age_rec = request.data["age_rec"]
+        game.player = player
+
+        # Use the Django ORM to get the record from the database
+        # whose `id` is what the client passed as the
+        # `gameTypeId` in the body of the request.
+
+
+        # Try to save the new game to the database, then
+        # serialize the game instance as JSON, and send the
+        # JSON as a response to the client request
+        try:
+            game.save()
+            serializer = GameSerializer(game, context={'request': request})
+            return Response(serializer.data)
+
+        # If anything went wrong, catch the exception and
+        # send a response with a 400 status code to tell the
+        # client that something was wrong with its request data
+        except ValidationError as ex:
+            return Response({"reason": ex.message}, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def list(self, request):
+        """Handle GET requests to games resource
+
+        Returns:
+            Response -- JSON serialized list of games
+        """
+        # Get all game records from the database
+        games = Game.objects.all()
+        
+        # Support filtering games by type
+        #    http://localhost:8000/games?type=1
+        #
+        # That URL will retrieve all tabletop games
+        # game_type = self.request.query_params.get('type', None)
+        # if game_type is not None:
+        #     games = games.filter(game_type__id=game_type)
+
+        serializer = GameSerializer(games, many=True, context={'request': request})
+        return Response(serializer.data)
+
+
+
+class GameSerializer(serializers.ModelSerializer):
+    """JSON serializer for games
+    Arguments: serializer type
+    """
+    class Meta:
+        model = Game
+        fields = ('id', 'title', 'description', 'designer',
+                    'year_released', 'number_of_players',
+                    'game_time', 'age_rec', 'player')
+        depth = 1
